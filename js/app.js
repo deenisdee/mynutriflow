712c046392ecc11c03d6167b9f25f799d0b62468
Denis Carvalho <47404461+deenisdee@users.noreply.github.com>
2025-12-31 05:28:07 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -896,7 +896,7 @@ async function showRecipeDetail(recipeId) {
     const sliderHeight = slider ? slider.offsetHeight : 400;
     
     window.scrollTo({ 
-      top: sliderHeight + 0, 
+      top: sliderHeight + 200, 
       behavior: 'smooth' 
     });
   }, 100);
